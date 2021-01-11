4e13a1d20008d343a92a90df52924532b17897fd
palesatau <you@example.com>
2021-01-11 16:01:32 +0200

    Add project files.

---
+++ Controllers/Task_DetailController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Database;
+using Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Task_DetailController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public Task_DetailController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Task_Detail
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Task_Detail>>> GetTask_Details()
+        {
+            return await _context.Task_Details.ToListAsync();
+        }
+
+        // GET: api/Task_Detail/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Task_Detail>> GetTask_Detail(int id)
+        {
+            var task_Detail = await _context.Task_Details.FindAsync(id);
+
+            if (task_Detail == null)
+            {
+                return NotFound();
+            }
+
+            return task_Detail;
+        }
+
+        // PUT: api/Task_Detail/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask_Detail(int id, Task_Detail task_Detail)
+        {
+            if (id != task_Detail.Task_ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(task_Detail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Task_DetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Task_Detail
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Task_Detail>> PostTask_Detail(Task_Detail task_Detail)
+        {
+            _context.Task_Details.Add(task_Detail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTask_Detail", new { id = task_Detail.Task_ID }, task_Detail);
+        }
+
+        // DELETE: api/Task_Detail/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Task_Detail>> DeleteTask_Detail(int id)
+        {
+            var task_Detail = await _context.Task_Details.FindAsync(id);
+            if (task_Detail == null)
+            {
+                return NotFound();
+            }
+
+            _context.Task_Details.Remove(task_Detail);
+            await _context.SaveChangesAsync();
+
+            return task_Detail;
+        }
+
+        private bool Task_DetailExists(int id)
+        {
+            return _context.Task_Details.Any(e => e.Task_ID == id);
+        }
+    }
+}
---
+++ Controllers/User_AccountController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Database;
+using Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class User_AccountController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public User_AccountController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/User_Account
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User_Account>>> GetUser_Accounts()
+        {
+            return await _context.User_Accounts.ToListAsync();
+        }
+
+        // GET: api/User_Account/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User_Account>> GetUser_Account(int id)
+        {
+            var user_Account = await _context.User_Accounts.FindAsync(id);
+
+            if (user_Account == null)
+            {
+                return NotFound();
+            }
+
+            return user_Account;
+        }
+
+        // PUT: api/User_Account/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser_Account(int id, User_Account user_Account)
+        {
+            if (id != user_Account.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user_Account).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!User_AccountExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/User_Account
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<User_Account>> PostUser_Account(User_Account user_Account)
+        {
+            _context.User_Accounts.Add(user_Account);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser_Account", new { id = user_Account.Id }, user_Account);
+        }
+
+        // DELETE: api/User_Account/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User_Account>> DeleteUser_Account(int id)
+        {
+            var user_Account = await _context.User_Accounts.FindAsync(id);
+            if (user_Account == null)
+            {
+                return NotFound();
+            }
+
+            _context.User_Accounts.Remove(user_Account);
+            await _context.SaveChangesAsync();
+
+            return user_Account;
+        }
+
+        private bool User_AccountExists(int id)
+        {
+            return _context.User_Accounts.Any(e => e.Id == id);
+        }
+    }
+}
